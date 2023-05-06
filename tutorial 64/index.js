0c09c5eb521ecf42cfd27f5c9a92b50c3e8b0a94
Masum Billal <mbillal.eee@gmail.com>
2023-05-06 17:03:52 +0600

    variable and function in ES6

---
+++ tutorial 64/index.js
@@ -0,0 +1,54 @@
+//Variable concatation----------------
+// var x = 6;
+// if(true){
+//     var x = 5;
+// }
+// console.log(x);
+
+// let y = 6;
+// if(true){
+//     let y = 5;
+// }
+// console.log(y);
+
+// let z = 11;
+// for(let z=1; z<=10; z++){
+//     // console.log(z)
+// }
+// console.log(z);
+
+// const a = 12;
+// if (true) {
+//   a = 10;
+// }
+// console.log(a);
+
+
+
+// var b = 20;
+// var c = 30;
+// var d = c + b;
+// console.log('Sum is ' + d + '. the end');
+
+
+// ES6 Start -------------------
+let e = 30;
+let f = 30;
+let g = e + f;
+console.log(`Sum is ${g}. The end`);
+
+
+let name = 'Masum Billal';
+console.log(`I am ${name}. I am web developer`);
+
+// function add(x,y){
+//     var sum = x+y;
+//     console.log(sum);
+// }
+
+const add = (x,y) =>{
+    let sum = x + y;
+    console.log(sum);
+}
+add(20,20);
+
